8b0e30ca509d48e2e7c3754708a91cab9f922cc5
Jonas Rapp <jonas.rapp@cinteros.se>
2017-03-08 17:30:57 +0100

    Adding ContextLogger and NoFaxPlease

---
+++ eXtremePlugins/ContextLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JonasPluginBase;
+
+namespace eXtremePlugins
+{
+    public class ContextLogger : JonasPluginBase.JonasPluginBase
+    {
+        public override void Execute(JonasPluginBag bag)
+        {
+            // noop
+        }
+    }
+}
---
+++ eXtremePlugins/NoFaxPlease.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JonasPluginBase;
+using Microsoft.Xrm.Sdk;
+
+namespace eXtremePlugins
+{
+    public class NoFaxPlease : JonasPluginBase.JonasPluginBase
+    {
+        public override void Execute(JonasPluginBag bag)
+        {
+            if (!string.IsNullOrWhiteSpace(bag.TargetEntity.GetAttributeValue<string>("fax")))
+            {
+                throw new InvalidPluginExecutionException($"Fax? Really? This is {DateTime.Now.Year}, you know... ");
+            }
+            else
+            {
+                bag.Trace("No fax for {0}, this {1} is ready for the future.", bag.CompleteEntity.Name(bag, false), bag.PluginContext.PrimaryEntityName);
+            }
+        }
+    }
+}
